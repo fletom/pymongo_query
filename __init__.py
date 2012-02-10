61283697d7d9d4ab63bb79214cbd95d114cfb832
Fletcher Tomalty <fletcher@nomulous.com>
2012-02-10 13:13:30 -0500

    Added k.starts_with and k.ends_with

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
+import re
 
 class mongo_query(dict):
 	def __init__(self, query):
 		dict.__init__(self, query)
-	
+
 	def __and__(self, other):
 		return mongo_query(dict(list(self.items()) + list(other.items())))
+
 	def __or__(self, other):
 		return mongo_query({'$or': [self, other]})
 
@@ -12,23 +14,27 @@ class mongo_query(dict):
 class k(object):
 	def __init__(self, key):
 		self.key = key
-	
 
 	def __eq__(self, value):
 		return mongo_query({self.key: value})
 
 	def __neq__(self, value):
 		return mongo_query({self.key: {'$ne': value}})
-	
 
 	def __gt__(self, value):
 		return mongo_query({self.key: {'$gt': value}})
-	
 
 	def __gte__(self, value):
 		return mongo_query({self.key: {'$gte': value}})
+
 	def __lt__(self, value):
 		return mongo_query({self.key: {'$lt': value}})
 
 	def __lte__(self, value):
 		return mongo_query({self.key: {'$lte': value}})
+
+	def starts_with(self, string):
+		return mongo_query({self.key: {'$regex': '^' + re.escape(string)}})
+	
+	def ends_with(self, string):
+		return mongo_query({self.key: {'$regex': re.escape(string) + '$'}})
