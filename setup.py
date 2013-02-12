3783abffbbdc222b26543f3262a9788545da8df9
Fletcher Tomalty <fletcher@tomalty.com>
2013-02-12 18:47:08 -0500

    Finally add a setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'mongo_query',
+	version = '0.1.0',
+	packages = [ 'pymongo_query.mongo_query' ],
+	url = 'https://github.com/fletom/pymongo_query',
+	license = '',
+	author = 'fletcher',
+	author_email = 'fletcher@tomalty.com',
+	description = 'A tiny Python library for writing more succinct and readable PyMongo/MongoDB queries.'
+)
